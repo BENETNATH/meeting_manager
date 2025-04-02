4bbebbb0d4bb17951d8a6245a7995d57f101590a
BENETNATH <benetnath@gmail.com>
2025-04-02 16:33:47 +0000

    Translations updated. Babel script for extraction on linux fixed

--- babel.sh
+++ babel.sh
@@ -1,3 +1,4 @@
-pybabel extract --ignore-dirs=".venv build" -F babel.cfg -o messages.pot .
+pybabel extract --ignore-dirs=.venv -F babel.cfg -o messages.pot .
 pybabel update -i messages.pot -d translations
+read -p "Now, translate every .po files located in translations\xx\LC_MESSAGES Then come back and press a key !"
 pybabel compile -d translations
